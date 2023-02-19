ca093c18ab3c915ea2deafec8b605dd747005ad5
Bruno Felix <bruno.felix@fuze.cc>
2023-02-19 20:46:00 -0300

    :truck: Resources added and updated

---
+++ app/src/main/java/dev/brunofelix/marvelapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package dev.brunofelix.marvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.brunofelix.marvelapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
